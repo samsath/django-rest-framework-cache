1c69a91dbac6707a61b5a708fb2e1d3b77c8549b
Sam Hipwell <samhipwell@gmail.com>
2017-04-10 14:47:49 +0100

    Update apps.py

--- rest_framework_cache/apps.py
+++ rest_framework_cache/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 class rest_cacheConfig(AppConfig):
     name='rest_framework_cache'
     verbose_name='rest_framework_cache'
+    path='/srv/riverside/website/.env/local/lib/python2.7/site-packages/rest_framework_cache'
